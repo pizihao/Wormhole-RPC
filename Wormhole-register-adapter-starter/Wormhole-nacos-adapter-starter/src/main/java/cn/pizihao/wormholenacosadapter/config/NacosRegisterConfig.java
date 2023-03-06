e0de7b9a4f3f5931f15dd1017a3bd1d715c90ad3
pizihao <2335715300@qq.com>
2023-03-06 18:23:59 +0800

    fix : 将nacos的适配器调整为一个适配器的starter

--- Wormhole-register-adapter-starter/Wormhole-nacos-adapter-starter/src/main/java/cn/pizihao/wormholenacosadapter/config/NacosRegisterConfig.java
+++ Wormhole-register-adapter-starter/Wormhole-nacos-adapter-starter/src/main/java/cn/pizihao/wormholenacosadapter/config/NacosRegisterConfig.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(NacosDiscoveryProperties.class)
 public class NacosRegisterConfig {
 
-
     @Bean
     @ConditionalOnBean({NacosDiscoveryProperties.class, NacosServiceDiscovery.class})
     public NacosServiceDiscoveryAdapter serviceRegister(NacosServiceDiscovery nacosServiceDiscovery) {
