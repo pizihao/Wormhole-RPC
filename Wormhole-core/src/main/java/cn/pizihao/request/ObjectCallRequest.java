3d92a45f2f20bd50b95de0af660f6ee89156b8c5
pizihao <2335715300@qq.com>
2023-03-07 13:55:07 +0800

    feat : 对请求体和响应体提供基本的实现

--- Wormhole-core/src/main/java/cn/pizihao/request/ObjectCallRequest.java
+++ Wormhole-core/src/main/java/cn/pizihao/request/ObjectCallRequest.java
@@ -1,35 +1,89 @@
 package cn.pizihao.request;
 
+import java.util.Objects;
+
 /**
  * 对象传输传递数据
  */
 public class ObjectCallRequest implements Request {
 
+    String RID;
+
+    String className;
+
+    String methodName;
+
+    Class<?>[] parameterTypes;
+
+    Object[] parameters;
+
+    boolean isReceive = true;
+
+    boolean isEventHeart = false;
+
+
+    public ObjectCallRequest(String RID, String className, String methodName, Class<?>[] parameterTypes, Object[] parameters, boolean isReceive) {
+        this.RID = RID;
+        this.className = className;
+        this.methodName = methodName;
+        this.parameterTypes = parameterTypes;
+        this.parameters = parameters;
+        this.isReceive = isReceive;
+    }
+
+    public ObjectCallRequest(String RID, String className, String methodName, Class<?>[] parameterTypes, Object[] parameters) {
+        this.RID = RID;
+        this.className = className;
+        this.methodName = methodName;
+        this.parameterTypes = parameterTypes;
+        this.parameters = parameters;
+    }
+
+    public ObjectCallRequest(String RID, boolean isEventHeart) {
+        this.RID = RID;
+        this.isEventHeart = isEventHeart;
+    }
+
     public String getRID() {
-        return null;
+        return RID;
     }
 
     public String getClassName() {
-        return null;
+        return className;
     }
 
     public String getMethodName() {
-        return null;
+        return methodName;
     }
 
     public Class<?>[] getParameterTypes() {
-        return new Class[0];
+        return parameterTypes;
     }
 
     public Object[] getParameters() {
-        return new Object[0];
+        return parameters;
     }
 
     public boolean isReceive() {
-        return false;
+        return isReceive;
     }
 
     public boolean isEventHeart() {
-        return false;
+        return isEventHeart;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        ObjectCallRequest that = (ObjectCallRequest) o;
+        return Objects.equals(RID, that.RID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(RID);
     }
 }
--- Wormhole-core/src/main/java/cn/pizihao/response/ObjectCallResponse.java
+++ Wormhole-core/src/main/java/cn/pizihao/response/ObjectCallResponse.java
@@ -1,35 +1,77 @@
 package cn.pizihao.response;
 
+import java.util.Objects;
+
 /**
  * 对象传输调用数据
  */
 public class ObjectCallResponse implements Response {
 
+    String RID;
+
+    Class<?> cls;
+
+    Object obj;
+
+    Throwable throwable;
+
+    String errMsg;
+
+    boolean isEventHeart = false;
+
+    public ObjectCallResponse(String RID, Class<?> cls, Object obj, Throwable throwable, String errMsg) {
+        this.RID = RID;
+        this.cls = cls;
+        this.obj = obj;
+        this.throwable = throwable;
+        this.errMsg = errMsg;
+    }
+
+    public ObjectCallResponse(String RID, boolean isEventHeart) {
+        this.RID = RID;
+        this.isEventHeart = isEventHeart;
+    }
+
     public String getRID() {
-        return null;
+        return RID;
     }
 
     public Class<?> getCls() {
-        return null;
+        return cls;
     }
 
     public Object getObj() {
-        return null;
+        return obj;
     }
 
     public Throwable getThrowable() {
-        return null;
+        return throwable;
     }
 
     public String getErrMsg() {
-        return null;
+        return errMsg;
     }
 
     public boolean isErr() {
-        return false;
+        return throwable != null || errMsg != null;
     }
 
     public boolean isEventHeart() {
-        return false;
+        return isEventHeart;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        ObjectCallResponse that = (ObjectCallResponse) o;
+        return Objects.equals(RID, that.RID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(RID);
     }
 }
