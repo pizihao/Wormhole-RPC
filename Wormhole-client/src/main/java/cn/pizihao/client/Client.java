d5ff619e79e91c096c824c3b2920c3a206707e88
pizihao <2335715300@qq.com>
2023-03-08 20:29:16 +0800

    feat : 重新定义客户端

--- Wormhole-client/src/main/java/cn/pizihao/client/Client.java
+++ Wormhole-client/src/main/java/cn/pizihao/client/Client.java
@@ -1,7 +1,14 @@
 package cn.pizihao.client;
 
+import java.io.Closeable;
+
 /**
- * 客户端
+ * 客户端<br>
+ * 这是定义客户端的顶级接口，其任务是连接服务器端，发送数据，接收数据，断开连接<br>
+ * 客户端需要一个连接的操作和一个断开的操作
  */
-public interface Client {
+public interface Client extends Closeable {
+
+
+
 }
