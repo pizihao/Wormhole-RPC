4eef4fd74a153cf5143d5ece0218c11072ced83e
pizihao <2335715300@qq.com>
2023-03-09 21:54:59 +0800

    feat : 简单的客户端实现

--- Wormhole-client/src/main/java/cn/pizihao/client/SimpleClient.java
+++ Wormhole-client/src/main/java/cn/pizihao/client/SimpleClient.java
@@ -1,4 +1,41 @@
 package cn.pizihao.client;
 
-public class SimpleClient {
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class SimpleClient implements Client {
+    @Override
+    public void connect() {
+
+    }
+
+    @Override
+    public InetSocketAddress getLocalAddress() {
+        return null;
+    }
+
+    @Override
+    public void send(Object message) {
+
+    }
+
+    @Override
+    public Object getAttribute(String key) {
+        return null;
+    }
+
+    @Override
+    public void setAttribute(String key, Object value) {
+
+    }
+
+    @Override
+    public boolean isActive() {
+        return false;
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
 }
