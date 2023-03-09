b61cb1baa5a48bc2e02d1d52ffa60243f3ee9027
pizihao <2335715300@qq.com>
2023-03-09 19:15:37 +0800

    fix : 调整回Closeable接口

--- Wormhole-server/src/main/java/cn/pizihao/server/Server.java
+++ Wormhole-server/src/main/java/cn/pizihao/server/Server.java
@@ -2,10 +2,12 @@ package cn.pizihao.server;
 
 import cn.pizihao.channel.Channel;
 
+import java.io.Closeable;
+
 /**
  * 服务器端
  */
-public interface Server extends Channel, AutoCloseable {
+public interface Server extends Channel, Closeable {
 
     /**
      * 绑定一个端口，同时启动服务
