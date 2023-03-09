b4806e805d8e141ab234a13b40357496b7301e4d
pizihao <2335715300@qq.com>
2023-03-09 14:55:02 +0800

    feat : 定义服务器端

--- Wormhole-server/src/main/java/cn/pizihao/Server.java
+++ Wormhole-server/src/main/java/cn/pizihao/Server.java
@@ -1,7 +1,16 @@
 package cn.pizihao;
 
+import cn.pizihao.channel.Channel;
+
+import java.io.Closeable;
+
 /**
  * 服务器端
  */
-public interface Server {
+public interface Server extends Channel, Closeable {
+
+    /**
+     * 绑定一个端口，同时启动服务
+     */
+    void bind();
 }
